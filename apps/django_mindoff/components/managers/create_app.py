898bdd7a8867b71a54520cfe5d7559e5c9e30c59
Joel Samraj <joelsamrajs@gmail.com>
2026-02-20 10:15:31 +0530

    updated create app logic url addition to use version kwargs

--- apps/django_mindoff/components/managers/create_app.py
+++ apps/django_mindoff/components/managers/create_app.py
@@ -128,7 +128,7 @@ class DjangoAppCreator:
         with open(self.urls_path, "r+") as f:
             content = f.read()
             url_prefix = self.original_path.split(".")[-1]
-            route = f"path('{url_prefix}/', include('{self.dotted_path}.urls')),"
+            route = f"path('v<int:version>/{url_prefix}/', include('{self.dotted_path}.urls')),"
             if route not in content:
                 print("[ACTION] Linking urls.py.")
                 lines = content.splitlines()
